b92b50e5b8a09435c53b7f17e234793004906b03
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2016-05-15 03:20:58 -0400

    As West and East in all flat maps (and I am one) are one,
    so death doth touch the Resurrection.

---
+++ spec/rhyme-view-spec.js
@@ -0,0 +1,10 @@
+/* eslint-env jasmine */
+'use babel'
+
+import RhymeView from '../lib/rhyme-view'
+
+describe('RhymeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy')
+  })
+})
